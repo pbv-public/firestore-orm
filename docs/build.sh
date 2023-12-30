fdde61797c8e108057f81745746ca3aad171bfc3
pbv <dev@pb.vision>
2023-12-30 12:17:00 -0800

    include git hash in the docs

--- docs/build.sh
+++ docs/build.sh
@@ -8,3 +8,7 @@ embed-markdown # Update .md files before embedding the .md files into docs
 cd "$script_path"
 rm -rf generated
 npx jsdoc --configure jsdoc.config.json `find ../src -name '*.js' -type f`
+
+gitHash=`git rev-parse HEAD`
+cat ./generated/index.html | sed -e "s/GIT_HASH/${gitHash}/g" > tmp
+mv tmp ./generated/index.html

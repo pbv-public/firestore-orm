834aefdecc21a333361a8816b94ec1c37507ea63
pbv <dev@pb.vision>
2024-01-27 12:00:00 -0800

    ensure test db is starting for tests
    too easy to forget and then it was confusing because tests would only
    fail when firestore's 60sec connection timeout failed (for each test,
    super slow!)

--- scripts/start-local-db.sh
+++ scripts/start-local-db.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-gcloud emulators firestore start --host-port [::1]:8404
+set -o errexit
+set -o nounset
+
+firestorePort=8404
+pid=`lsof -tPi :$firestorePort -sTCP:LISTEN || echo 'port_not_in_use'`
+if [ "$pid" != "port_not_in_use" ]; then
+    echo "local db emulator port $firestorePort already in use; replacing PID $pid"
+    kill $pid
+fi
+gcloud emulators firestore start --host-port [::1]:$firestorePort > /dev/null 2> /dev/null &
+echo "firestore db emulator is now running in the background as PID $!"
